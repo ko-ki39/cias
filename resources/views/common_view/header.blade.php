0e64dc88b8754d0ca6600cfd1fb12fc3ca3c2f2f
ko-ki39 <0yc3263109y278z@ezweb.ne.jp>
2020-09-07 08:01:39 +0900

    家での作業分

--- resources/views/common_view/header.blade.php
+++ resources/views/common_view/header.blade.php
@@ -1,17 +1,16 @@
-
-<div class="top">
+<div class="header">
+    <div class="top">
         <i class="far fa-bars"></i>
         <h2>Sign up</h2>
         <h2>Login</h2>
         <h2>Logout</h2>
-</div>
-<div class="header">
-    <h2><a href="">ポートフォリオページへ</a></h2>
-    <h2><a href="">ゲームページへ</a></h2>
-    <h2>------の生徒が作った作品を紹介するページです！！</h2>
-    <div class="ad">
-        {{-- 広告を入れる場所 --}}
+    </div>
+    <div class="header_bot">
+        <h2><a href="">ポートフォリオページへ</a></h2>
+        <h2><a href="">ゲームページへ</a></h2>
+        <h2>------の生徒が作った作品を紹介するページです！！</h2>
+        <div class="ad">
+            {{-- 広告を入れる場所 --}}
+        </div>
     </div>
 </div>
-
-
