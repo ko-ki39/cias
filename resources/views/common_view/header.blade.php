560c9390712394179facef88c1359f9751bb86e9
ko-ki39 <66869823+ko-ki39@users.noreply.github.com>
2020-09-07 16:16:19 +0900

    帰る前に

--- resources/views/common_view/header.blade.php
+++ resources/views/common_view/header.blade.php
@@ -1,16 +1,18 @@
-<div class="header">
-    <div class="top">
-        <i class="far fa-bars"></i>
+<div id="header">
+    <div id="top">
         <h2>Sign up</h2>
         <h2>Login</h2>
         <h2>Logout</h2>
     </div>
     <div class="header_bot">
-        <h2><a href="">ポートフォリオページへ</a></h2>
-        <h2><a href="">ゲームページへ</a></h2>
-        <h2>------の生徒が作った作品を紹介するページです！！</h2>
+        <div class="intro">
+            <h2><a href="">ポートフォリオページへ</a></h2>
+            <h2><a href="">ゲームページへ</a></h2>
+            <h2><br>------の生徒が作った作品を紹介するページです！！</h2>
+        </div>
         <div class="ad">
             {{-- 広告を入れる場所 --}}
         </div>
     </div>
 </div>
+<script src="{{ asset('/js/hamburger.js') }}"></script>
