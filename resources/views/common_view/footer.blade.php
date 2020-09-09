4d496b5b8ed9c9fa077b127a0ced2cbab1d52926
ko-ki39 <66869823+ko-ki39@users.noreply.github.com>
2020-09-09 11:01:14 +0900

    footer調整

--- resources/views/common_view/footer.blade.php
+++ resources/views/common_view/footer.blade.php
@@ -1,4 +1,7 @@
 <div class="footer">
+    <div class="foot_middle">
+
+    </div>
     <div class="foot_left">
         <p>訓練校からプログラマーを目指した<br>男たち（2人）が運営するwebサイトです！</p>
         <ul>
@@ -12,15 +15,16 @@
     <div class="foot_right">
         <p>お問い合わせ、掲載の依頼など</p>
         <p>ota</p>
-        <div class="twitter">
-            <div class="image"></div>
-            <p>ota　twitter</p>
+        <div class="foot_twitter">
+            <div class="foot_image"></div>
+            <p>ota twitter</p>
         </div>
+
         <p>ko-ki</p>
-        <div class="twitter">
-            <div class="image"></div>
-            <p>ko-ki　twitter</p>
+        <div class="foot_twitter">
+            <div class="foot_image"></div>
+            <p>ko-ki twitter</p>
         </div>
-        <p>サイト開設日 2020/00/00</p>
+        <small>サイト開設日 2020/00/00</small>
     </div>
 </div>
