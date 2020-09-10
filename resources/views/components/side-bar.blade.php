bb7b75a9865f82f24fd668be1e128c4b37f4986a
ko-ki39 <66869823+ko-ki39@users.noreply.github.com>
2020-09-10 15:44:56 +0900

    サイドバー作成

---
+++ resources/views/components/side-bar.blade.php
@@ -0,0 +1,15 @@
+<div class="side_bar">
+    <h2>最新の記事</h2>
+    <div class="latest_article">
+        <div class="side_image"></div>
+        <p>タイトル</p>
+        <p class="detail">１２３４５６７８９１２３４５６７８９１２３４５６７８９１２３４５６７８９１２３４５６７８９</p>
+        <p class="date">投稿日付 2020/00/00</p>
+    </div>
+    <div class="latest_article">
+        <div class="side_image"></div>
+        <p>タイトル</p>
+        <p class="detail">詳細</p>
+        <p class="date">投稿日付 2020/00/00</p>
+    </div>
+</div>
