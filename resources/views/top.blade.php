c147a3fab54b8db36538736a39c9f1bc5401fe7e
ko-ki39 <66869823+ko-ki39@users.noreply.github.com>
2020-09-14 16:31:15 +0900

    途中

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -3,6 +3,7 @@
 @section('import')
 {{-- css等の読み込み場所 --}}
 <link rel="stylesheet" href="/css/side_bar.css" type="text/css">
+<link rel="stylesheet" href="/css/top.css" type="text/css">
 @endsection
 
 @section('content')
@@ -11,6 +12,30 @@
     @component('components.article')
     {{-- 記事一覧表示場所 --}}
     @endcomponent
+    <div class="content">
+        <div class="article">
+            <div class="article_image">
+            </div>
+            <p class="article_title">タイトル</p>
+            <p class="article_description">詳細00000000000000000000000000000000000000</p>
+            <p class="date">2020/00/00</p>
+            <div class="comment">
+            </div>
+            <div class="twitter"></div>
+            <div class="fav"></div>
+        </div>
+        <div class="article">
+            <div class="article_image">
+            </div>
+            <p class="article_title">タイトル</p>
+            <p class="article_description">詳細</p>
+            <p class="date">2020/00/00</p>
+            <div class="comment">
+            </div>
+            <div class="twitter"></div>
+            <div class="fav"></div>
+        </div>
+    </div>
     @component('components.side-bar')
     {{-- ここはサイドバーです --}}
     @endcomponent
