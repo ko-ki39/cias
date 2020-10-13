252d3a034c2c350280ca9f301fe9c82f26a8822e
ko-ki39 <66869823+ko-ki39@users.noreply.github.com>
2020-10-13 09:12:21 +0900

    ハッシュ機能途中

--- public/js/hash.js
+++ public/js/hash.js
@@ -1,6 +1,12 @@
 var search = document.getElementById('search');
 search.addEventListener('keyup', function() {
     if (search.value == '#') {
-
+        $.ajax({
+            type: 'GET',
+            url: 'top/' + hashTag,
+            data: {
+                'hashTag':
+            }
+        })
     }
 })
