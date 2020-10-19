0d2bc3f833962d4bdd6c0f1237690f59a2c77ad4
ko-ki39 <66869823+ko-ki39@users.noreply.github.com>
2020-10-19 15:58:55 +0900

    人気の記事一覧のUI変更

--- resources/views/components/side-bar.blade.php
+++ resources/views/components/side-bar.blade.php
@@ -1,19 +1,33 @@
 <div class="side_bar">
     <h2>人気の記事</h2>
-    @foreach($articles_ranking as $article_ranking)
-    <div class="latest_article">
-        <img src="/storage/{{ $article_ranking->image }}" class="side_image">
-        <a href="{{ route('article_detail', ['id' => $article_ranking->id]) }}">
-            <p>{{ $article_ranking->title }}</p>
-        </a>
-        <p class="detail">{{ $article_ranking->description }}</p>
-        <a href="{{ route('individual', ['id' => \App\User::find($article_ranking->user_id)->id]) }}">
-            <p class="user_name">
-                {{ \App\User::find($article_ranking->user_id)->user_name }}
-            </p>
-        </a>
-        <p class="date">投稿日付 {{ $article_ranking->created_at }}</p>
-    </div>
+    @foreach ($articles_ranking as $article_ranking)
+        {{-- <div class="latest_article">
+            <div class="image_div">
+                <img src="/storage/{{ $article_ranking->image }}" class="side_image"></div>
+            <a href="{{ route('article_detail', ['id' => $article_ranking->id]) }}">
+                <p>{{ $article_ranking->title }}</p>
+            </a>
+            <a href="{{ route('individual', ['id' => \App\User::find($article_ranking->user_id)->id]) }}">
+                <p class="user_name">
+                    {{ \App\User::find($article_ranking->user_id)->user_name }}
+                </p>
+            </a>
+            <p class="date">投稿日付 {{ $article_ranking->created_at }}</p>
+        </div> --}}
+        <div class="latest_article">
+            <div class="l_a_img">
+                <img src="/storage/{{ $article_ranking->image }}" alt="">
+            </div>
+            <div class="l_a_detail">
+                <a href="{{ route('article_detail', ['id' => $article_ranking->id]) }}">
+                    {{ $article_ranking->title }}
+                </a>
+                <a href="{{ route('individual', ['id' => \App\User::find($article_ranking->user_id)->id]) }}">
+                    投稿者：{{ \App\User::find($article_ranking->user_id)->user_name }}
+                </a>
+                <p>投稿日時&nbsp;{{ $article_ranking->created_at }}</p>
+            </div>
+        </div>
 
     @endforeach
 </div>
