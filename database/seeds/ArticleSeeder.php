16814742c191f917e5b0b8a6cf2fabd704829be0
ko-ki39 <66869823+ko-ki39@users.noreply.github.com>
2020-10-26 09:31:36 +0900

    seeder修正

--- database/seeds/ArticleSeeder.php
+++ database/seeds/ArticleSeeder.php
@@ -19,6 +19,10 @@ class ArticleSeeder extends Seeder
             'title' => '内臓が溶ける！？実は危険な市販薬５選！',
             'description' => 'こんにちは、皆さん！　元気ですか！？<br>ストレス社会が加速するこのご時世、なかなか気力を保つことは難しいと思います。そんな皆さんに、お医者さんから処方してもらえる、元気が出るお薬を紹介します。(合法)',
             'image' => 'rhetw46j',
+            'hash1_id' => 'ハッシュ1',
+            'hash2_id' => 'ハッシュ2',
+            'hash3_id' => 'ハッシュ3',
+            'created_at' => '2020-10-16 06:28:50',
         ];
         DB::table('articles')->insert($param);
 
@@ -27,6 +31,10 @@ class ArticleSeeder extends Seeder
             'title' => '【Unity】コルーチンの使い方を５つの例で紹介！',
             'description' => '説明2',
             'image' => 'rhetw46j',
+            'hash1_id' => 'ハッシュ4',
+            'hash2_id' => 'ハッシュ2',
+            'hash3_id' => 'ハッシュ3',
+            'created_at' => '2020-10-17 06:28:50',
         ];
         DB::table('articles')->insert($param);
 
@@ -35,6 +43,10 @@ class ArticleSeeder extends Seeder
             'title' => 'タイトル3',
             'description' => '説明3',
             'image' => 'rhetw46j',
+            'hash1_id' => 'ハッシュ5',
+            'hash2_id' => 'ハッシュ2',
+            'hash3_id' => 'ハッシュ3',
+            'created_at' => '2020-10-18 06:28:50',
         ];
         DB::table('articles')->insert($param);
     }
