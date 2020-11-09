e073707e9e97b0cf0a9b248b92c53e5d016eb676
ko-ki39 <66869823+ko-ki39@users.noreply.github.com>
2020-11-09 15:58:59 +0900

    fav.js変更

--- public/js/fav.js
+++ public/js/fav.js
@@ -1,6 +1,3 @@
-
-
-
 let yjsnpi = document.getElementsByClassName("tippy_template")[0].innerHTML;
 let mur = document.getElementsByClassName("tippy_template")[1].innerHTML;
 let kmr = document.getElementsByClassName("tippy_template")[2].innerHTML;
@@ -21,55 +18,57 @@ let nowURL = location.href;
 // $(".heart-button-l").on("click", function(){
 //     alert("great!!!");
 // });
-if(nowURL.substring(0,40) == "http://127.0.0.1:8000/top/article_detail"){
-    buttons[0].addEventListener("click", function(e){
-        
+if (nowURL.substring(0, 40) == "http://127.0.0.1:8000/top/article_detail" ||
+    nowURL.substring(0, 35) == "http://localhost/top/article_detail") {
+    buttons[0].addEventListener("click", function(e) {
+
         // 塗り潰しされてない(favされてない、またはログインしていない)
-        if(buttons[0].classList[4] == "far"){
+        if (buttons[0].classList[4] == "far") {
             fav(0, "create");
 
-         // 塗り潰しされてる(過去にお気に入りした)
-        }else if(buttons[0].classList[4] == "fas"){
+            // 塗り潰しされてる(過去にお気に入りした)
+        } else if (buttons[0].classList[4] == "fas") {
             fav(0, "delete");
         }
     }, true);
-}else if(nowURL.substring(0,25) == "http://127.0.0.1:8000/top"){
-    for(let i=0; i<article.length; i++){
-        buttons[i].addEventListener("click", function(e){
+} else if (nowURL.substring(0, 25) == "http://127.0.0.1:8000/top" ||
+    nowURL.substring(0, 20) == "http://localhost/top") {
+    for (let i = 0; i < article.length; i++) {
+        buttons[i].addEventListener("click", function(e) {
             // console.log(e.target.classList);
             // console.log(buttons[i].classList);
-            
+
             // 塗り潰しされてない(favされてない、またはログインしていない)
-            if(buttons[i].classList[4] == "far"){
+            if (buttons[i].classList[4] == "far") {
                 fav(i, "create");
                 // requestTest();
                 // 塗り潰しされてる(過去にお気に入りした)
-            }else if(buttons[i].classList[4] == "fas"){
+            } else if (buttons[i].classList[4] == "fas") {
                 fav(i, "delete");
             }
         }, true);
     }
 }
 
-for(let j=0; j<article.length; j++){
-    buttons[j].addEventListener("mouseover", function(e){
-        if(buttons[j].classList[3] == "tippyLoginFav"){
+for (let j = 0; j < article.length; j++) {
+    buttons[j].addEventListener("mouseover", function(e) {
+        if (buttons[j].classList[3] == "tippyLoginFav") {
             tippy_L();
-        }else if(buttons[j].classList[3] == "tippyGuestFav"){
+        } else if (buttons[j].classList[3] == "tippyGuestFav") {
             tippy_G();
         }
     });
-    buttons2[j].addEventListener("mouseover", function(e){
-        if(buttons[j].classList[3] == "tippyLoginFav"){
+    buttons2[j].addEventListener("mouseover", function(e) {
+        if (buttons[j].classList[3] == "tippyLoginFav") {
             tippy_L_Twitter();
-        }else if(buttons[j].classList[3] == "tippyGuestFav"){
+        } else if (buttons[j].classList[3] == "tippyGuestFav") {
             tippy_G_Twitter();
         }
     });
-    buttons3[j].addEventListener("mouseover", function(e){
-        if(buttons[j].classList[3] == "tippyLoginFav"){
+    buttons3[j].addEventListener("mouseover", function(e) {
+        if (buttons[j].classList[3] == "tippyLoginFav") {
             tippy_L_Comment();
-        }else if(buttons[j].classList[3] == "tippyGuestFav"){
+        } else if (buttons[j].classList[3] == "tippyGuestFav") {
             tippy_G_Comment();
         }
     });
@@ -82,7 +81,7 @@ for(let j=0; j<article.length; j++){
  */
 // select   :   選択した記事のID(整数)
 // m_string :   イベントリスナーのクロージャーから受け取ったメソッド名(文字列)
-function fav(select, m_string){
+function fav(select, m_string) {
     let article_id = document.getElementsByClassName("article_ajax_id")[select].value;
     let _method = m_string;
 
@@ -92,47 +91,47 @@ function fav(select, m_string){
     formData.append("p_method", _method)
 
     $.ajaxSetup({
-        headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')}
+        headers: { 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content') }
     })
     $.ajax({
-        url: "/top/fav/" + article_id,
-        method: "post",
-        data: formData,
-        // data: {"p_article_id": article_id, "p_method": _method},
-        processData: false,
-        contentType: false,
-    })
-    // resData: ControllerからresponseしたJSONデータ
-    //          中身はControllerを参照してください
-    .done(function(resData, jqXHR){
-        if(resData.success_flag == "ok"){
-            switch(resData.p_method){
-                case "create":
-                    buttons[select].classList.remove("far");
-                    buttons[select].classList.add("fas");
-                    break;
-                case "delete":
-                    buttons[select].classList.remove("fas");
-                    buttons[select].classList.add("far");
-                    break;
-                case "exists":
-                    break;
+            url: "/top/fav/" + article_id,
+            method: "post",
+            data: formData,
+            // data: {"p_article_id": article_id, "p_method": _method},
+            processData: false,
+            contentType: false,
+        })
+        // resData: ControllerからresponseしたJSONデータ
+        //          中身はControllerを参照してください
+        .done(function(resData, jqXHR) {
+            if (resData.success_flag == "ok") {
+                switch (resData.p_method) {
+                    case "create":
+                        buttons[select].classList.remove("far");
+                        buttons[select].classList.add("fas");
+                        break;
+                    case "delete":
+                        buttons[select].classList.remove("fas");
+                        buttons[select].classList.add("far");
+                        break;
+                    case "exists":
+                        break;
+                }
+                console.log(resData.message);
+                console.log("method is: " + resData.p_method);
+                console.log("article_id id: " + resData.p_article_id);
+            } else if (resData.success_flag == "plz_login") {
+                console.log(resData.message);
+            } else if (resData.success_flag == "omg") {
+                console.log(resData.message);
             }
-            console.log(resData.message);
-            console.log("method is: " + resData.p_method);
-            console.log("article_id id: " + resData.p_article_id);
-        }else if(resData.success_flag == "plz_login"){
-            console.log(resData.message);
-        }else if(resData.success_flag == "omg"){
-            console.log(resData.message);
-        }
-    })
-    .fail(function(error){
-        console.log("通信エラー", error);
-    })
+        })
+        .fail(function(error) {
+            console.log("通信エラー", error);
+        })
 }
 
-function tippy_L(){
+function tippy_L() {
     tippy('.tippyLoginFav', {
         content: yjsnpi,
         allowHTML: true,
@@ -142,7 +141,7 @@ function tippy_L(){
     });
 }
 
-function tippy_G(){
+function tippy_G() {
     tippy('.tippyGuestFav', {
         content: yjsnpi,
         allowHTML: true,
@@ -152,7 +151,7 @@ function tippy_G(){
     });
 }
 
-function tippy_L_Twitter(){
+function tippy_L_Twitter() {
     tippy('.twitter-button-l', {
         content: mur,
         allowHTML: true,
@@ -162,7 +161,7 @@ function tippy_L_Twitter(){
     });
 }
 
-function tippy_G_Twitter(){
+function tippy_G_Twitter() {
     tippy('.twitter-button-l', {
         content: mur,
         allowHTML: true,
@@ -172,7 +171,7 @@ function tippy_G_Twitter(){
     });
 }
 
-function tippy_L_Comment(){
+function tippy_L_Comment() {
     tippy('.comment-button-l', {
         content: kmr,
         allowHTML: true,
@@ -182,7 +181,7 @@ function tippy_L_Comment(){
     });
 }
 
-function tippy_G_Comment(){
+function tippy_G_Comment() {
     tippy('.comment-button-l', {
         content: kmr,
         allowHTML: true,
