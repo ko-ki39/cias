0725c4f7eed7f0d844b25b54737ca9eaffc6139e
ko-ki39 <66869823+ko-ki39@users.noreply.github.com>
2020-11-13 13:23:36 +0900

    generate.bladeのhtmlちょっと追加

--- resources/views/generate.blade.php
+++ resources/views/generate.blade.php
@@ -2,25 +2,29 @@
 
 @section('content')
     {{-- アカウント生成用のページ --}}
-    <form action="{{ route('generate') }}">
-        <th>学科</th>
-        <th>人数</th>
-        <th>年</th>
-        <td><select name="department">
-                <option value="1">管理者権限 1</option>
-                <option value="2" selected>投稿許可権限 2</option>
-                <option value="1">管理者権限 1</option>
-                <option value="2" selected>投稿許可権限 2</option>
-                <option value="1">管理者権限 1</option>
-                <option value="2" selected>投稿許可権限 2</option>
-                <option value="1">管理者権限 1</option>
-                <option value="2" selected>投稿許可権限 2</option>
-            </select>
-        </td>
-        <td><input type="number" name="num" id="num"></td>
-        <td><select name="age">
-            <option value="1">
-                </option>
-        </select></td>
-    </form>
+    <th>学科</th>
+    <th>年</th>
+    <th>人数</th>
+    <th>期限</th>
+    <tr>
+        <form action="{{ route('generate') }}">
+            <td><select name="department">
+                    <option value="1">オフィスビジネス科（前期）</option>
+                    <option value="2">オフィスビジネス科（後期）</option>
+                    <option value="3">自動車整備科</option>
+                    <option value="4">電気システム科</option>
+                    <option value="5">メディア・アート科</option>
+                    <option value="6">情報システム科</option>
+                    <option value="7">造園ガーデニング科</option>
+                    <option value="8">総合実務科（知的障がい者対象）</option>
+                </select>
+            </td>
+            <td><select name="age">
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                </select></td>
+            <td><input type="number" name="num" id="num"></td>
+                <input type="submit" value="作成">
+        </form>
+    </tr>
 @endsection
