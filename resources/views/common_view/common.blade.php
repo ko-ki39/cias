993b8e61cd3b27ebda296e586e28a6a6967238b7
ko-ki39 <66869823+ko-ki39@users.noreply.github.com>
2020-11-30 11:04:00 +0900

    コメントへのgood処理保存のみ完了

--- resources/views/common_view/common.blade.php
+++ resources/views/common_view/common.blade.php
@@ -10,7 +10,6 @@
         {{-- <link href="{{ asset('/css/hamburger.css') }}" rel="stylesheet"> --}}
         <link rel="stylesheet" href="/css/hamburger.css" type="text/css">
         <link rel="stylesheet" href="/css/css_tippy/shift-toward-extreme.css">
-        @yield('import')
         {{-- <link href="/css/fontawesome_v561.css" rel="stylesheet"> --}}
         <script src="/js/fontawesome0853445863.js" crossorigin="anonymous"></script>
         <script src="/js/jquery-3.5.1.min.js"></script>
@@ -18,6 +17,7 @@
         <script src="/js/popper.min.js"></script>
         <script src="/js/tippy-bundle.umd.js"></script>
         <script src="/js/confirm.js"></script>
+        @yield('import')
         {{-- <script src="https://unpkg.com/tippy.js@6/dist/tippy-bundle.umd.js"></script> --}}
     </head>
     <body>
