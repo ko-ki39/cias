b41f088e2f1b6d5e32e7d682c9e7f7aa187df2cc
otkotk <ser1ous.bo.36dm@gmail.com>
2020-12-23 00:43:49 +0900

    fav.jsを修正

--- public/js/welcome_quietflow.js
+++ public/js/welcome_quietflow.js
@@ -1,5 +1,16 @@
-$("body").quietflow({
-    theme : "starfield",
-    starSize : 1.25,
-    speed : 60
-});
+// $("body").quietflow({
+//     theme : "starfield",
+//     starSize : 1.25,
+//     speed : 60
+// });
+
+function hoge() {
+    $("body").quietflow({
+        theme: "starfield",
+        starSize: 1.2,
+        speed: 60
+    });
+}
+
+window.onload = hoge();
+window.addEventListener("resize", hoge, true);
