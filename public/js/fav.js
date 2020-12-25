2bc8b128d8b2c4998814bbdd59d698f1c52d783c
ko-ki39 <66869823+ko-ki39@users.noreply.github.com>
2020-12-25 09:37:02 +0900

    Revert "fav.js治りました"
    This reverts commit 20cfb86afb23b4c6a210643f7b81f957046e8548.

--- public/js/fav.js
+++ public/js/fav.js
@@ -8,9 +8,9 @@ let buttons2 = document.getElementsByClassName("twitter-button-l");
 let buttons3 = document.getElementsByClassName("comment-button-l");
 let article = document.getElementsByClassName("article");
 let nowURL = location.href;
+let nowHost = location.hostname;
 let nowHostOrigin = location.origin;
-// console.log(nowURL.substring(0, 25));
-// console.log(nowHostOrigin + "/top");
+// console.log(buttons);
 // buttons[0].classList.remove("far");
 // buttons[0].classList.add("fas");
 
@@ -22,43 +22,37 @@ let nowHostOrigin = location.origin;
 // $(".heart-button-l").on("click", function(){
 //     alert("great!!!");
 // });
-//https://127.0.0.1:8000/top/article_detail
-//https://localhost/top/article_detail
-//https://cias-gushi.herokuapp.com/top/article_detail
-if (nowURL.substring(0, 40) == nowHostOrigin + "/top/article_detail" ||
-    nowURL.substring(0, 39) == nowHostOrigin + "/top/article_detail" ||
-    nowURL.substring(0, 36) == nowHostOrigin + "/top/article_detail" ||
-    nowURL.substring(0, 35) == nowHostOrigin + "/top/article_detail" ||
-    nowURL.substring(0, 51) == nowHostOrigin + "/top/article_detail" ||
-    nowURL.substring(0, 50) == nowHostOrigin + "/top/article_detail") {
+console.log(nowURL.substring(0, 40));
+console.log("http://" + nowHost + "/top");
+console.log(nowHostOrigin + "/top");
+if(nowURL.substring(0, 40) == "https://http:/cias-gushi.herokuapp.com/top/article_detail" ||
+    nowURL.substring(0, 40) == "https://http:/cias-gushi.herokuapp.com/top/article_detail"){
     buttons[0].addEventListener("click", function(e) {
 
+        console.lop("読み込めてない！");
         // 塗り潰しされてない(favされてない、またはログインしていない)
-        if (buttons[0].classList[4] == "far") {
+        if(buttons[0].classList[4] == "far"){
             fav(0, "create");
 
             // 塗り潰しされてる(過去にお気に入りした)
-        } else if (buttons[0].classList[4] == "fas") {
+        }else if(buttons[0].classList[4] == "fas"){
             fav(0, "delete");
         }
     }, true);
-} else if (nowURL.substring(0, 25) == nowHostOrigin + "/top" ||
-            nowURL.substring(0, 24) == nowHostOrigin + "/top" ||
-            nowURL.substring(0, 21) == nowHostOrigin + "/top" ||
-            nowURL.substring(0, 20) == nowHostOrigin + "/top" ||
-            nowURL.substring(0, 36) == nowHostOrigin + "/top" ||
-            nowURL.substring(0, 35) == nowHostOrigin + "/top") {
-    for (let i = 0; i < article.length; i++) {
-        buttons[i].addEventListener("click", function(e) {
+}else if(nowURL.substring(0, 25) == "https://http:/cias-gushi.herokuapp.com/top" ||
+        nowURL.substring(0, 25) == "https://http:/cias-gushi.herokuapp.com/top"){
+    for (let i = 0; i < article.length; i++){
+        buttons[i].addEventListener("click", function(e){
             // console.log(e.target.classList);
             // console.log(buttons[i].classList);
 
+            console.log("きた");
             // 塗り潰しされてない(favされてない、またはログインしていない)
-            if (buttons[i].classList[4] == "far") {
+            if(buttons[i].classList[4] == "far"){
                 fav(i, "create");
                 // requestTest();
                 // 塗り潰しされてる(過去にお気に入りした)
-            } else if (buttons[i].classList[4] == "fas") {
+            }else if(buttons[i].classList[4] == "fas"){
                 fav(i, "delete");
             }
         }, true);
@@ -109,7 +103,7 @@ function fav(select, m_string) {
         headers: { 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content') }
     })
     $.ajax({
-            url: "/top/fav/" + article_id,
+            url: "/top/fav/",
             method: "post",
             data: formData,
             // data: {"p_article_id": article_id, "p_method": _method},
